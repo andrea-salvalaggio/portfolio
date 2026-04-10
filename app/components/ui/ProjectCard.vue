a0336611bef7c8003aff02d83e7e88e219c8ee88
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2026-04-10 22:44:02 +0200

    style: update ProjectCard component to use consistent heading class for project title

--- app/components/ui/ProjectCard.vue
+++ app/components/ui/ProjectCard.vue
@@ -3,7 +3,7 @@
         <div class="card h-100 border-0">
             <img :src="project.image" :alt="project.title" class="card-image rounded-1 lazyload" />
             <div class="card-body d-flex flex-column p-0 mt-3">
-                <div class="card-title fw-medium">{{ project.title }}</div>
+                <div class="h5 card-title fw-medium">{{ project.title }}</div>
                 <p class="card-text fw-light">{{ project.description }}</p>
             </div>
         </div>
