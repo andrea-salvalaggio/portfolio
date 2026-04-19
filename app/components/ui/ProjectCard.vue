9aa40145e3065787cb4baf5da28718ce2b7b0620
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2026-04-19 14:53:18 +0200

    feat: adjust ProjectCard image layout and transition effects for improved responsiveness

--- app/components/ui/ProjectCard.vue
+++ app/components/ui/ProjectCard.vue
@@ -1,8 +1,8 @@
 <template>
     <a :href="project.url" class="card-link text-decoration-none" :aria-label="`View ${project.title}`">
         <div class="card flex-row-reverse align-items-center flex-sm-column h-100 bg-transparent border-0 pb-4 pb-sm-0">
-            <div class="card-image">
-                <img :src="project.image" :alt="project.title" class="rounded-1 col-4 col-sm-12 lazyload" />
+            <div class="card-image col-4 col-sm-12">
+                <img :src="project.image" :alt="project.title" class="rounded-1 lazyload h-100 w-100" />
             </div>
             <div class="card-body d-flex flex-column p-0 mt-sm-3 pe-3 pe-sm-0">
                 <div class="h5 card-title fw-medium mb-1 mb-sm-2">{{ project.title }}</div>
@@ -30,12 +30,7 @@ defineProps({
 
         img {
             height: fit-content;
-            transition: transform 0.5s cubic-bezier(0.25, 1, 0.5, 1);
-        
-            @media (max-width: 576px) {
-                height: 100%;
-                object-fit: cover;
-            }
+            transition: transform 0.6s cubic-bezier(0.25, 1, 0.5, 1);
         }
     }
 
