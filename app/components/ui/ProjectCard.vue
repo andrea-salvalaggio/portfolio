770ad4da5e7c219ebf50ef88c43470d7dc2d3aa4
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2026-04-19 14:48:48 +0200

    feat: restructure ProjectCard component layout and enhance image hover effects

--- app/components/ui/ProjectCard.vue
+++ app/components/ui/ProjectCard.vue
@@ -1,7 +1,9 @@
 <template>
     <a :href="project.url" class="card-link text-decoration-none" :aria-label="`View ${project.title}`">
         <div class="card flex-row-reverse align-items-center flex-sm-column h-100 bg-transparent border-0 pb-4 pb-sm-0">
-            <img :src="project.image" :alt="project.title" class="card-image rounded-1 col-4 col-sm-12 lazyload" />
+            <div class="card-image">
+                <img :src="project.image" :alt="project.title" class="rounded-1 col-4 col-sm-12 lazyload" />
+            </div>
             <div class="card-body d-flex flex-column p-0 mt-sm-3 pe-3 pe-sm-0">
                 <div class="h5 card-title fw-medium mb-1 mb-sm-2">{{ project.title }}</div>
                 <p class="card-text line-clamp-2 line-clamp-sm-3 fw-light">{{ project.description }}</p>
@@ -22,12 +24,27 @@ defineProps({
 <style scoped lang="scss">
 @use '@/assets/scss/variables' as *;
 
-.card-image {
-    height: fit-content;
+.card {
+    .card-image {
+        overflow: clip;
 
-    @media (max-width: 576px) {
-        height: 100%;
-        object-fit: cover;
+        img {
+            height: fit-content;
+            transition: transform 0.5s cubic-bezier(0.25, 1, 0.5, 1);
+        
+            @media (max-width: 576px) {
+                height: 100%;
+                object-fit: cover;
+            }
+        }
+    }
+
+    &:hover {
+        .card-image {
+            img {
+                transform: scale(1.08);
+            }
+        }
     }
 }
 
