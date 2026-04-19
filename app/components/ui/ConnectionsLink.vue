d1d016b683842b49ee79f670c85aca66067f909c
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2026-04-19 23:11:41 +0200

    feat: remove redundant btn class from ConnectionsLink and StackLink components for consistency

--- app/components/ui/ConnectionsLink.vue
+++ app/components/ui/ConnectionsLink.vue
@@ -1,5 +1,5 @@
 <template>
-    <a :href="connection.url" class="btn btn-link fw-light d-flex align-items-center justify-content-between text-decoration-none roundend-1 w-100" target="_blank" :aria-label="connection.label">
+    <a :href="connection.url" class="btn-link fw-light d-flex align-items-center justify-content-between text-decoration-none roundend-1 w-100" target="_blank" :aria-label="connection.label">
         <div class="d-flex align-items-center">
             <font-awesome-icon :icon="connection.icon" class="fs-5 me-2" /> {{ connection.label }} 
         </div>
