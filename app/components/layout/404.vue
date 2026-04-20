330423862b9b2b36476dd61588315d25064a5759
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2026-04-20 23:54:29 +0200

    feat: create custom 404 error page

---
+++ app/components/layout/404.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="error-section">
+        <div class="container p-0">
+            <div class="col-12">
+                <h1 class="error-title fw-bold">404</h1>
+                <p class="mb-4">
+                    Ops! The page you are looking for does not exist <font-awesome-icon :icon="['fas', 'face-frown']" class="ms-1"/> <br>
+                    Please check the URL or return to the homepage.
+                </p>
+                <NuxtLink to="/">Return home</NuxtLink>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+@use '@/assets/scss/variables' as *;
+@import "bootstrap/scss/bootstrap";
+
+.error-title {
+    @include media-breakpoint-down(sm) {
+        font-size: $font-size-big;
+    }
+}
+
+a {
+    color: $dark
+}
+</style>
---
+++ app/error.vue
@@ -0,0 +1,12 @@
+<template>
+    <LayoutAppHeader />
+    <main class="p-3 py-5 py-sm-8">
+        <Layout404 />
+    </main>
+</template>
+
+<script setup>
+useHead({
+    title: '404 | Andrea Salvalaggio'
+})
+</script>
